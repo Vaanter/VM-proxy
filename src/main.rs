09c0f4aa7b0590a3c11ebf049d2cbc6074c3f5ab
Valentín Bolfík <valentin.bolfik@gmail.com>
2025-04-02 22:07:19 +0200

    Remove network proxy

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-use crate::proxy::Proxy;
 use crate::sqlite_proxy::main_loop;
 use config::Config;
 use once_cell::sync::Lazy;
 use sqlite::SqliteConnection;
 use sqlx::{Connection, sqlite};
-use std::collections::BTreeMap;
 use std::sync::Arc;
-use std::time::Duration;
 use tokio::signal::ctrl_c;
 use tokio::sync::Mutex;
-use tokio::time::timeout;
-use tokio_util::sync::CancellationToken;
 use tracing::{error, info};
 use tracing_subscriber::Registry;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
-mod proxy;
 mod sqlite_proxy;
 mod utils;
 
@@ -65,57 +59,6 @@ async fn run() {
       // if let Err(e) = timeout(Duration::from_secs(5), handle).await {
       //   error!("Task failed to exit in time");
       // }
-    }
-    Err(e) => error!("Ctrl-c signal error! {e}"),
-  }
-}
-
-async fn run_listeners() {
-  let proxies = CONFIG
-    .get_array("proxies")
-    .expect("Config must define proxies!")
-    .into_iter()
-    .map(|p| p.into_table().unwrap())
-    .collect::<Vec<_>>();
-
-  let cancel = CancellationToken::new();
-  let mut tasks = BTreeMap::new();
-
-  for (index, proxy_map) in proxies.iter().enumerate() {
-    let upstream = proxy_map.get("upstream").map(|u| u.to_string());
-    let downstream = proxy_map.get("downstream").map(|u| u.to_string());
-    match (upstream, downstream) {
-      (Some(upstream), Some(downstream)) => {
-        let cancel = cancel.clone();
-        info!("Creating proxy {} -> {}", &upstream, &downstream);
-        let proxy = Proxy::new(upstream, downstream);
-        tasks.insert(
-          index,
-          tokio::spawn(async move {
-            if let Err(e) = proxy.listener_loop(cancel.clone()).await {
-              error!("Listener {} failed: {}", index, e);
-            }
-          }),
-        );
-      }
-      (_, _) => {
-        error!(
-          "Proxy at position {} is missing required keys {{'upstream', 'downstream'}}",
-          index
-        );
-      }
-    }
-  }
-
-  match ctrl_c().await {
-    Ok(()) => {
-      info!("Ctrl-c received!");
-      cancel.cancel();
-      for handle in tasks.iter_mut() {
-        if let Err(e) = timeout(Duration::from_secs(5), handle.1).await {
-          error!("Listener {} failed to exit in time", handle.0);
-        }
-      }
     }
     Err(e) => error!("Ctrl-c signal error! {e}"),
   }
